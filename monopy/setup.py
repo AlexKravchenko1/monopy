b7b522e7f4e3658eee7231ecb3f9a7e214e56d24
Oleksii Kravchenko <41645696+AlexKravchenko1@users.noreply.github.com>
2023-01-05 16:26:46 +0000

    Added setup.py module in monopy according to monopy-1

---
+++ monopy/setup.py
@@ -0,0 +1,13 @@
+"""a module used to build and distribute Python packages for MonoBank API."""
+from setuptools import setup
+
+setup(
+    name='monopy',
+    version='0.0.1',
+    description='A Python package for Monobank API integration',
+    author='Oleksii Kravchenko',
+    author_email='andibull25@gmail.com',
+    packages=['monopy'],
+    install_requires=[
+    ],
+)
