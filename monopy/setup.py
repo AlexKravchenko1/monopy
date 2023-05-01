3730ec60c4f2083f575aa79436ecbe9fba22affd
Oleksii Kravchenko <41645696+AlexKravchenko1@users.noreply.github.com>
2023-05-01 22:44:18 +0300

    Changed description

--- monopy/setup.py
+++ monopy/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='MonoAPI',
     version='0.1',
-    description='Python package for monobank API',
+    description='A Python package for Monobank API integration',
     author='Oleksii Kravchenko',
     author_email='andibull@gmail.com',
     packages=find_packages(),
